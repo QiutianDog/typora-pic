f78f5bb0781aad7c76aa49ea143bdcbef80b3c5c
langxi.feng <langxi.feng@hand-china.com>
2022-09-12 16:26:45 +0800

    [ADD] add config

---
+++ config.py
@@ -0,0 +1,25 @@
+# Gitea config
+
+# http or https
+protocol = "http"
+
+# ip address or domain
+domain = ""
+
+# port default 3000
+port = 3000
+
+# your gitea username
+owner = ""
+
+# your gitea repo name
+repo = ""
+
+# your gitea repo branch, default "master"
+branch = "master"
+
+# where your file upload to repo filepath
+filepath = "uPic/"
+
+# your gitea Access Token
+token = ""
